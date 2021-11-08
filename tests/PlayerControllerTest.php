d3cbe5ef7b8e9a1608bd8e7d01ef097981c2525c
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2021-11-08 16:34:44 +0100

    ajout des commentaires de tests

--- tests/PlayerControllerTest.php
+++ tests/PlayerControllerTest.php
@@ -57,18 +57,27 @@ class PlayerControllerTest extends WebTestCase
         $this->assertIdentifier();
     }
 
+    /**
+     * Test d'un mauvais identifier;
+     */
     public function testBadIdentifier()
     {
         $this->client->request('GET', '/player/display/badIdentifier');
         $this->assertError404($this->client->getResponse()->getStatusCode());
     }
 
+    /**
+     * Test d'un identifier non existant;
+     */
     public function testInexistingIdentifier()
     {
         $this->client->request('GET', '/player/display/7ba48618db8e2108a57604108ff9bc835a3error');
         $this->assertError404($this->client->getResponse()->getStatusCode());
     }
 
+    /**
+     * Test d'une modification de player;
+     */
     public function testModify() 
     {
         $this->client->request('PUT', '/player/modify/' . self::$identifier);
@@ -76,27 +85,42 @@ class PlayerControllerTest extends WebTestCase
         $this->assertIdentifier();
     }
 
+    /**
+     * Test d'une suppression de player;
+     */
     public function testDelete() 
     {
         $this->client->request('DELETE', '/player/delete/' . self::$identifier);
         $this->assertEquals(500, $this->client->getResponse()->getStatusCode());
     }
 
+    /**
+     * Test d'assertion d'un identifier
+     */
     public function assertIdentifier()
     {
         $this->assertArrayHasKey('identifier', $this->content);
     }
 
+    /**
+     * Définition de l'identifier
+     */
     public function defineIdentifier()
     {
         self::$identifier = $this->content['identifier'];
     }
 
+    /**
+     * Test d'assertion d'une erreur 404
+     */
     public function assertError404($statusCode)
     {
         $this->assertEquals(404, $statusCode);
     }
 
+    /**
+     * Test d'assertion d'une réponse JSON
+     */
     public function assertJsonResponse()
     {
         $response = $this->client->getResponse();
